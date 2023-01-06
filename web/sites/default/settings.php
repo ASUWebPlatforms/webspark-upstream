ad4d6e91556fa57070c61adea058f237c45579df
Bryan R <aubjr@asu.edu>
2023-01-06 13:43:17 -0700

    Updated settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -26,6 +26,24 @@ include __DIR__ . "/settings.pantheon.php";
 // $settings['skip_permissions_hardening'] = TRUE;
 
 /**
+ * Enable the configuration readonly functionality.
+ */
+if (PHP_SAPI !== 'cli') {
+  $settings['config_readonly'] = TRUE;
+}
+
+/**
+ * Allow all configuration to be changed.
+ */
+$settings['config_readonly_whitelist_patterns'] = ['*'];
+
+/**
+ * Allow files to be deleted from the file system, similar to Drupal 7.
+ * See: https://www.drupal.org/node/2891902
+ */
+$config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
+
+/**
  * If there is a local settings file, then include it
  */
 $local_settings = __DIR__ . "/settings.local.php";
