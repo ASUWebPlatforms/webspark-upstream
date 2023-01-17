827bef03e7dbf8bbcc62c8bd81826356f241c447
Alexander Persky <apersky@asu.edu>
2023-01-17 08:51:59 -0700

    Remove duplicated code

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -44,24 +44,6 @@ $settings['config_readonly_whitelist_patterns'] = ['*'];
 $config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
 
 /**
- * Enable the configuration readonly functionality.
- */
-if (PHP_SAPI !== 'cli') {
-  $settings['config_readonly'] = TRUE;
-}
-
-/**
- * Allow all configuration to be changed.
- */
-$settings['config_readonly_whitelist_patterns'] = ['*'];
-
-/**
- * Allow files to be deleted from the file system, similar to Drupal 7.
- * See: https://www.drupal.org/node/2891902
- */
-$config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
-
-/**
  * If there is a local settings file, then include it
  */
 $local_settings = __DIR__ . "/settings.local.php";
